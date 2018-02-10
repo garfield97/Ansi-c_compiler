43720e8ffb1f18ad7607e70567ac4f2a34312531
mahudu97 <mahuduhd@gmail.com>
2018-02-10 19:20:04 +0000

    set up for parser

---
+++ src/ast.hpp
@@ -0,0 +1,12 @@
+#ifndef ast_hpp
+#define ast_hpp
+
+#include "ast_expression.hpp"
+#include "ast_primitives.hpp"
+#include "ast_operators.hpp"
+#include "ast_functions.hpp"
+
+
+extern const Expression *parseAST();
+
+#endif
