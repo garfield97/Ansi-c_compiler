4c99d513365cfc67709a5e63b8d8782ad3cc8907
mahudu97 <mahuduhd@gmail.com>
2018-02-19 12:11:30 +0000

    root

--- src/ast.hpp
+++ src/ast.hpp
@@ -1,12 +1,13 @@
 #ifndef ast_hpp
 #define ast_hpp
 
+#include "ast/ast_unit_translation.hpp"
 #include "ast/ast_expression.hpp"
 #include "ast/ast_primitives.hpp"
 #include "ast/ast_operators.hpp"
 #include "ast/ast_functions.hpp"
 
 
-extern const Expression *parseAST();
+extern const Unit_Translation *parseAST();
 
 #endif
