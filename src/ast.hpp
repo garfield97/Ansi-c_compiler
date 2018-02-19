08ab70ec133149a07b8026ee13bd29525cab0b1a
mahudu97 <mahuduhd@gmail.com>
2018-02-19 12:45:40 +0000

    classes

--- src/ast.hpp
+++ src/ast.hpp
@@ -2,7 +2,19 @@
 #define ast_hpp
 
 #include "ast/ast_unit_translation.hpp"
-#include "ast/ast_expression.hpp"
+#include "ast/ast_declaration.hpp"
+#include "ast/ast_function_defenition.hpp"
+#include "ast/ast_specifier_qualifier.hpp"
+#include "ast/ast_statements.hpp"
+#include "ast/ast_expressions.hpp"
+#include "ast/ast_constants.hpp"
+
+#include "ast/ast_.hpp"
+#include "ast/ast_.hpp"
+#include "ast/ast_.hpp"
+#include "ast/ast_.hpp"
+#include "ast/ast_.hpp"
+#include "ast/ast_.hpp"
 #include "ast/ast_primitives.hpp"
 #include "ast/ast_operators.hpp"
 #include "ast/ast_functions.hpp"
