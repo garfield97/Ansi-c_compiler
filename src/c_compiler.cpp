fe58f8e08c8ca94815c0100cb2bec0d400425a62
mahudu97 <mahuduhd@gmail.com>
2018-02-20 04:33:53 +0000

    int main - dealing with source file argument

--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -1,11 +1,31 @@
+#include <stdio.h>
+
 #include "ast.hpp"
 
-int main(int argc, char[] argv)
+int main(int argc, char* argv[])
 {
-    const Unit_Translation *ast=parseAST();
+    if(argc < 4){
+        fprintf(stderr, "incorrect argument amount\n");
+        exit(1);
+    }
+    if(argv[2] != "-o"){
+        fprintf(stderr, "third argument is not '-o'\n");
+        exit(1);
+    }
 
-    ast->print(std::cout);
-    std::cout<<std::endl;
+    // Build AST (FE)
+    // 2nd argument is source C file to compile
+    FILE* fp = fopen(argv[1], "r");
+    const Node* ast = parseAST(fp);
+    fclose(fp);
+
+
+    // Compile to Assembly
+    // bin/c_compiler -S [source-file.c] -o [dest-file.s]
+   
+
+    // Translate to Python
+    // bin/c_compiler --translate [source-file.c] -o [dest-file.py]
 
     return 0;
 }
