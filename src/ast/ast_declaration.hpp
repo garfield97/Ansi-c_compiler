b7d61f51ac0743cf6246bad2eed1957191d2efee
mahudu97 <mahuduhd@gmail.com>
2018-02-22 01:26:24 +0000

    overloading constructors
    can we just use arg1 as names?:
    - don't really care what it is
    - the overloading of base Node deals with correctly outputing?

--- src/ast/ast_declaration.hpp
+++ src/ast/ast_declaration.hpp
@@ -35,201 +35,126 @@ class declaration_external : public Node {
 };
 
 
-
-
-
 class definition_function : public Node{
+    private:
+        NodePtr  specifier_declaration;
+        NodePtr  declarator;
+        NodePtr  list_declaration;
+        NodePtr  statement_compound;
 
+    protected:
+        definition_function(NodePtr _arg1,NodePtr _arg2,NodePtr_arg3,NodePtr _arg4)
+            :specifier_declaration(_arg1)
+            ,declarator(_arg2)
+            ,list_declaration(_arg3)
+            ,statement_compound(_arg4)
+        {}
 
+        definition_function(NodePtr _arg1,NodePtr _arg2, char x, NodePtr_arg3) // char used to differentiate
+            :specifier_declaration(_arg1)
+            ,declarator(_arg2)
+            ,list_declaration(NULL)
+            ,statement_compound(_arg3)
+        {}
 
-private:
+        definition_function(char x, NodePtr _arg_1,NodePtr _arg_2,NodePtr _arg_3) // char used to differentiate
+            :specifier_declaration(NULL)
+            ,declarator(_arg_1)
+            ,list_declaration(_arg_2)
+            ,statement_compound(_arg_3)
+        {} 
+     
+        definition_function(NodePtr _arg1,NodePtr _arg2)
+            :specifier_declaration(NULL)
+            ,declarator(_arg1)
+            ,list_declaration(NULL)
+            ,statement_compound(_arg2)
+        {}
 
-    NodePtr  specifier_declaration;
-    NodePtr  declarator;
-    NodePtr  list_declaration;
-    NodePtr  statement_compound;
+    public:
 
+        virtual void PrettyPrint(std::ostream &dst) const override
+        {
+            if(next != null) next->print();
+            declaration->print();
+        }
 
-protected:
-    
+        virtual void toPYPY(std::ostream &dst) const override{
 
-    definition_function(NodePtr _arg1,NodePtr _arg2,NodePtr_arg3,NodePtr _arg4)
-        :specifier_declaration(_arg1)
-        ,declarator(_arg2)
-        ,list_declaration(_arg3)
-        ,statement_compound(_arg4)
-    {}
+        }
 
+        virtual void renderASM(std::ostream &dst) const override{
 
-    definition_function(NodePtr _arg1,NodePtr _arg2,NodePtr_arg3)
-        :specifier_declaration(_arg1)
-        ,declarator(_arg2)
-        ,list_declaration(NULL)
-        ,statement_compound(_arg3)
-    {}
-
-
-
-    definition_function(NodePtr _arg_1,NodePtr _arg_2,NodePtr _arg_3)
-        :specifier_declaration(NULL)
-        ,declarator(_arg_1)
-        ,list_declaration(_arg_2)
-        ,statement_compound(_arg_3)
-    {}
- 
- 
- 
- 
-    definition_function(NodePtr _arg1,NodePtr _arg2)
-        :specifier_declaration(NULL)
-        ,declarator(_arg1)
-        ,list_declaration(NULL)
-        ,statement_compound(_arg2)
-    {}
- 
-
-
-
-public:
-
-
-    virtual void PrettyPrint(std::ostream &dst) const override
-    {
-        if(next != null) next->print();
-        declaration->print();
-    }
-
-    virtual void toPYPY(std::ostream &dst) const override{
-
-    }
-
-    virtual void renderASM(std::ostream &dst) const override{
-
-    }
-
-
-
+        }
 };
 
 
-
 class declaration : public Node{
+    private:
+        NodePtr  specifier_declaration;
+        NodePtr  declarator_list_init;
+        
+    protected:      
+        declaration(NodePtr _arg1)
+            :specifier_declaration
+            ,declarator_list_init(NULL)
+        {}
+        
+        declaration(NodePtr _arg1,NodePtr _arg2) 
+            :specifier_declaration(_arg1)
+            ,declarator_list_init(_arg2)
+        {}
 
-private:
+    public:
+        virtual void PrettyPrint(std::ostream &dst) const override
+        {
+            if(next != null) {next->print();
+                // SPECIFIER_DECLARATION DECLARATOR_INIT_LIST ';'
+            }
+            // SPECIFIER_DECLARATION ';'
+            declaration->print();
+        }
 
-    NodePtr  specifier_declaration;
-    NodePtr  declarator_list_init;
-    char semicolon_in
-    
-protected:
-    
-    declaration(NodePtr _arg1,char semicolon)
-        :specifier_declaration
-        ,declarator_list_init(NULL)
-        ,semicolon_in(semicolon)
-    {}
-    
-    
-    declaration(NodePtr _arg1,NodePtr _arg2,char semicolon) 
-        :specifier_declaration(_arg1)
-        ,declarator_list_init(_arg2)
-        ,semicolon_in(semicolon)
-    {}
+        virtual void toPYPY(std::ostream &dst) const override{
 
+        }
 
+        virtual void renderASM(std::ostream &dst) const override{
 
-public:
-
-    virtual void PrettyPrint(std::ostream &dst) const override
-    {
-        if(next != null) next->print();
-        declaration->print();
-    }
-
-    virtual void toPYPY(std::ostream &dst) const override{
-
-    }
-
-    virtual void renderASM(std::ostream &dst) const override{
-
-    }
-
+        }
 };
 
 
 
 class specifier_declaration : public Node{
+    private:
+        NodePtr spec_qali;
+        NodePtr next;
+        
+    protected:
+        specifier_declaration(NodePtr _arg1)
+            :spec_qali(_arg1)
+            ,next(NULL)
+        {}
+        specifier_declaration(NodePtr _arg1,NodePtr _arg2)
+            :spec_qali(_arg1)
+            ,next(_arg2)
+        {}    
+        
+    public:
+        virtual void PrettyPrint(std::ostream &dst) const override
+        {
+            if(next != null) next->print();
+            declaration->print();
+        }
 
-private:
-    
-    NodePtr specifier_store_class;
-    NodePtr specifier_declaration;
-    NodePtr specifier_type;
-    NodePtr qualifier_type;
-    
+        virtual void toPYPY(std::ostream &dst) const override{
 
+        }
 
+        virtual void renderASM(std::ostream &dst) const override{
 
-protected:
-
-//Not sure if the functional overloading actually works properly though
-    specifier_declaration(NodePtr _arg1)
-        :specifier_store_class(_arg1)
-        ,specifier_declaration(NULL)
-        ,specifier_type(NULL)
-        ,qualifier_type(NULL)
-    {}
-    specifier_declaration(NodePtr _arg1,NodePtr _arg2)
-        :specifier_store_class(_arg1)
-        ,specifier_declaration(_arg2)
-        ,specifier_type(NULL)
-        ,qualifier_type(NULL)
-    {}    
-    
-    specifier_declaration(NodePtr _arg_1)
-        :specifier_store_class(NULL)
-        ,specifier_declaration(NULL)
-        ,specifier_type(_arg_1)
-        ,qualifier_type(NULL)
-    {}    
-    specifier_declaration(NodePtr _arg_1,NodePtr _arg_2)
-        :specifier_store_class(NULL)
-        ,specifier_declaration(_arg_1)
-        ,specifier_type(_arg_2)
-        ,qualifier_type(NULL)
-    {}
-    
-    specifier_declaration(NodePtr _arg1__)
-        :specifier_store_class(NULL)
-        ,specifier_declaration(NULL)
-        ,specifier_type(NULL)
-        ,qualifier_type(_arg1__)
-    {}        
-    specifier_declaration(NodePtr _arg1__,NodePtr _arg2__)
-        :specifier_store_class(NULL)
-        ,specifier_declaration(_arg2__)
-        ,specifier_type(NULL)
-        ,qualifier_type(_arg1__)
-    {}    
-
-
-
-public:
-
-    virtual void PrettyPrint(std::ostream &dst) const override
-    {
-        if(next != null) next->print();
-        declaration->print();
-    }
-
-    virtual void toPYPY(std::ostream &dst) const override{
-
-    }
-
-    virtual void renderASM(std::ostream &dst) const override{
-
-    }
-
+        }
 };
 
 
