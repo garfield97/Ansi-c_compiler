a98076a332fe4246856db6f9a89c3858aa2d07a6
garfield97 <rz1616@ic.ac.uk>
2018-02-23 12:19:01 +0000

    statement done

--- src/ast/ast_statement.hpp
+++ src/ast/ast_statement.hpp
@@ -250,7 +250,8 @@ public:
             else{
                 labels->PrettyPrint(dst);
                 dst<<':';    
-                statement->PrettyPrint(dst);                
+                statement->PrettyPrint(dst);     
+            }           
         }
 
         virtual void toPY(std::ostream &dst) const override{
@@ -260,13 +261,184 @@ public:
         virtual void renderASM(std::ostream &dst) const override{
 
         }
+};
+
+
+
+
+class statement_jump : public Node{
+    
+//STATEMENT_JUMP : GOTO IDENTIFIER ';'
+//               | CONTINUE ';'
+//               | BREAK ';'
+//               | RETURN ';'
+//               | RETURN EXPR ';'
+private:
+    NodePtr expr;
+    std::string symbol;
+    std::string symbol_2;
+protected:
+    
+    statement_jump(std::string name_1,std::string name_2)
+        :symbol(name_1)
+        ,symbol_2(name_2)
+    {}
+    
+    statement_jump(std::string name_1)
+        :symbol(name_1)
+        ,symbol_2(NULL)
+    {}    
+    
+    statement_jump(std::string name_1,NodePtr _arg)
+        :symbol(name_1)
+        ,symbol_2(NULL)
+        ,expr(_arg)
+    {}
+public:
+
+        std::string name = "statement_jump";
+    
+        virtual void PrettyPrint(std::ostream &dst) const override
+        {
+            if(expr != NULL){
+                symbol->PrettyPrint(dst);
+                expr->PrettyPrint(dst);
+                dst<<';';    
+            }
+            else if(symbol_2 != NULL){
+                symbol->PrettyPrint(dst);
+                symbol_2->PrettyPrint(dst);  
+                dst<<';';        
+            }           
+            else{
+                symbol->PrettyPrint(dst);
+                dst<<';';
+        }
+
+        virtual void toPY(std::ostream &dst) const override{
+
+        }
+
+        virtual void renderASM(std::ostream &dst) const override{
+
+        }
+};
+
+
+class statement_iteration : public Node{
+//STATEMENT_ITERATION : WHILE L_BRACKET EXPR R_BRACKET STATEMENT
+//                    | DO STATEMENT WHILE L_BRACKET EXPR R_BRACKET ';' 
+//                    | FOR L_BRACKET STATEMENT_EXPR STATEMENT_EXPR R_BRACKET STATEMENT
+//                    | FOR L_BRACKET STATEMENT_EXPR STATEMENT_EXPR EXPR R_BRACKET STATEMENT
+private:
+    
+    NodePtr expr;
+    NodePtr statement;
+    NodePtr statement_expr;
+    std::string symbol;
+    std::string symbol_1;
+    NodePtr statement_expr_rep;
+protected:
+    statement_iteration(std::string name,NodePtr _arg1,NodePtr _arg2)
+        :symbol(name)
+        ,expr(_arg1)
+        ,statement(_arg2)
+        ,symbol_1(NULL)
+        ,statement_expr(NULL)
+        ,statement_expr_rep(NULL)
+    {}
+    statement_iteration(std::string name,NodePtr _arg1,std::string name_1,NodePtr _arg2)
+        :symbol(name)
+        ,expr(_arg2)
+        ,statement(_arg1)
+        ,symbol_1(name_1)
+        ,statement_expr(NULL)
+        ,statement_expr_rep(NULL)
+    {}
+    statement_iteration(std::string name,NodePtr _arg,NodePtr _arg1,NodePtr _arg2)
+        :symbol(name)
+        ,expr(NULL)
+        ,statement(_arg2)
+        ,symbol_1(NULL)
+        ,statement_expr(_arg0)
+        ,statement_expr_rep(arg1)
+
+    {} 
+    statement_iteration(std::string name,NodePtr _arg,NodePtr _arg1,NodePtr _arg2,NodePtr _arg3)
+        :symbol(name)
+        ,expr(_arg3)
+        ,statement(_arg2)
+        ,symbol_1(NULL)
+        ,statement_expr(_arg0)
+        ,statement_expr_rep(arg1)
+    {}
+public:
+        std::string name = "statement_iteration";
+    
+        virtual void PrettyPrint(std::ostream &dst) const override
+        {
+            if(symbol_1!= NULL){
+                symbol->PrettyPrint(dst);
+                statement->PrettyPrint(dst);
+                symbol_1->PrettyPrint(dst);
+                expr->PrettyPrint(dst);
+                dst<<';';
+            }
+            else if(expr == NULL){
+                symbol->PrettyPrint(dst);
+                statement_expr->PrettyPrint(dst);
+                statement_expr_rep->PrettyPrint(dst);
+                statement->PrettyPrint(dst);
+                dst<';';
+            }           
+            else if(symbol_1 == NULL && statement_expr == NULL) {
+                symbol->PrettyPrint(dst);
+                dst<<'(';
+                expr->PrettyPrint(dst);
+                dst<<')'<<';';
+            }
+            else{
+                symbol->PrettyPrint(dst);
+                dst<<'(';
+                statement_expr->PrettyPrint(dst);
+                statement_expr_rep->PrettyPrint(dst);
+                expr->PrettyPrint(dst);
+                dst<<')';
+                statement->PrettyPrint(dst);
+            }
+        }
+
+        virtual void toPY(std::ostream &dst) const override{
+
+        }
+
+        virtual void renderASM(std::ostream &dst) const override{
+
+        }
+};
+
+class statement_selection : public Node{
+
+//STATEMENT_SELECTION : IF L_BRACKET EXPR R_BRACKET STATEMENT
+//                   | IF L_BRACKET EXPR R_BRACKET STATEMENT ELSE STATEMENT
+//                    | SWITCH L_BRACKET EXPR R_BRACKET STATEMENT
+
+
+private:
 
 
 
 
 
+protected
 
-           
+
+
+
+public
+
+
+
 
 
 #endif
