0965842dce59d8bc33698e0e82a48f52c2bf34c8
mahudu97 <mahuduhd@gmail.com>
2018-02-23 17:31:54 +0000

    fix

--- src/ast.hpp
+++ src/ast.hpp
@@ -2,14 +2,12 @@
 #define ast_hpp
 
 #include "ast/ast_node.hpp"
-
 #include "ast/ast_program.hpp"
 #include "ast/ast_declaration.hpp"
 #include "ast/ast_function_definition.hpp"
 #include "ast/ast_specifier_qualifier.hpp"
 #include "ast/ast_statement.hpp"
 #include "ast/ast_expression.hpp"
-#include "ast/ast_terminal.hpp"
 
 
 
