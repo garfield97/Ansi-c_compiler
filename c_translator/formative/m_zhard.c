4531895f3768b36f1ef9800b2cd2b7001a25f7d5
mahudu97 <mahuduhd@gmail.com>
2018-02-27 22:24:32 +0000

    fix m_zhard test to ansi c

--- c_translator/formative/m_zhard.c
+++ c_translator/formative/m_zhard.c
@@ -30,22 +30,24 @@ int oprOnGlobal(){
 
 int main(int argc)
 {
+	int count = 5;
+	int res;
+	
     setGlobal();
-
-    int counts = 5;
-    while(counts > 2){
+    
+    while(count > 2){
         addToGlobal(1,1);
-        if(counts == 4 || counts==5){
+        if(count == 4 || count==5){
           addToGlobal(1,1);  
         }
         else{
-            if(counts+3 == 6) addToGlobal(3,3); 
+            if(count+3 == 6) addToGlobal(3,3); 
             else addToGlobal(0,0);
         }
-		counts = counts -1;
+		count = count -1;
     }
 
-    int res = oprOnGlobal();
+    res = oprOnGlobal();
     if(res >0 && res< 256){
         return res;
     }
