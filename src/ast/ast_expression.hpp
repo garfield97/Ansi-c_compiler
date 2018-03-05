9fd8ae397dcc910794562beeddd94700b1e74f53
garfield97 <rz1616@ic.ac.uk>
2018-03-05 15:33:05 +0000

    basic expr done

--- src/ast/ast_expression.hpp
+++ src/ast/ast_expression.hpp
@@ -34,8 +34,10 @@ class expr : public Node {
 
         virtual void compile(std::ostream &dst, CompileContext &context) const override
         {
-            dst<<"AST Node: "<<name<<" does not yet support compile function"<<std::endl;
-            exit(1);
+        
+            rec->compile(dst,context);
+            assignment->compile(dst,context);
+            
         }
 };
 
