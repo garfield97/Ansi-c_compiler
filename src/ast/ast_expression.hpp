36158193a3120f993104da38845c9f23ebaf68a0
garfield97 <rz1616@ic.ac.uk>
2018-03-08 14:24:07 +0000

    some weird codes fixed

--- src/ast/ast_expression.hpp
+++ src/ast/ast_expression.hpp
@@ -117,18 +117,21 @@ class expr_assignment : public Node {
         virtual void compile(std::ostream &dst, CompileContext &context) const override
         {
             unary->compile(dst,context);                                //Find the name of the variable stored in expression_result
+                
             binding tmp_binding;                                            //create a temporary bindings for later use
             tmp_binding = context.scopes[context.scope_index][context.expr_result];        //setting the temp binding to equals toquery the expr_results
 
-            int reg_assign = context.get_free_reg();                           // getting a free register for use
-           
-            if(reg_assign == 33){                                                   //checking if there are actually free registers
+            int reg_assign = tmp_binding.reg_ID                           // getting a free register for use
+               
+            if(reg_assign == 33){     
+                                                          //checking if there are actually free registers            
                 context.free_up_reg();                                              // free up the registers by loading them onto the stack for use
                 reg_assign = context.get_free_reg();
+                tmp_binding.reg_ID = reg_assign;
+                
+                
             }
-            
-            tmp_binding.reg_ID = reg_assign;
-            
+
             context.scopes[context.scope_index][context.expr_result] = tmp_binding; //updating the binding stored in our vectors of map-> no more updates to reg_assign
             
                     
--- src/ast/ast_statement.hpp
+++ src/ast/ast_statement.hpp
@@ -380,8 +380,26 @@ class statement_jump : public Node{
             if(expr != NULL) {
                                      
                 expr->compile(dst, context); // prints out an immediate or a register value
+                
+                
+                binding tmp_binding;                                            //create a temporary bindings for later use
+                tmp_binding = context.scopes[context.scope_index][context.expr_result];        //setting the temp binding to equals toquery the expr_results
+
+                int reg_assign = tmp_binding.reg_ID                           // getting a free register for use
+               
+                if(reg_assign == 33){                                                   //checking if there are actually free registers
+                    context.free_up_reg();                                              // free up the registers by loading them onto the stack for use
+                    reg_assign = context.get_free_reg();
+                    tmp_binding.reg_ID = reg_assign;
+                }
+            
+
+            
+                context.scopes[context.scope_index][context.expr_result] = tmp_binding; //updating the binding stored in our vectors of map-> no more updates to reg_assign
+            
                                     
-                dst<<"\taddi\t$2,$0,"<<context.expr_result<<"\n";
+                dst<<"\tadd\t$2,$0,$,"<<   <<"\n";
+                
         
 
             }
