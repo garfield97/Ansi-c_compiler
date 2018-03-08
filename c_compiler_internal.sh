f802fbdf5ba871fc5ac8b06cceec7c909f13566e
mahudu97 <mahuduhd@gmail.com>
2018-03-08 12:40:11 +0000

    New script to test compiler internally

---
+++ c_compiler_internal.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [[ "$1" != "" ]] ; then
+    compiler="$1"
+else
+    compiler="bin/c_compiler"
+fi
+
+have_compiler=0
+if [[ ! -f bin/c_compiler ]] ; then
+    >&2 echo "Warning : cannot find compiler at path ${compiler}. Only checking C reference against python reference."
+    have_compiler=1
+fi
+
+input_dir="c_compiler_internal"
+
+working="tmp/internal"
+mkdir -p ${working}
+
+for i in ${input_dir}/*.c ; do
+    base=$(echo $i | sed -E -e "s|${input_dir}/([^.]+)[.]c|\1|g");
+    
+    # Compile the reference C version
+    gcc $i -o $working/$base
+    
+    # Run the reference C version
+    $working/$base
+    REF_C_OUT=$?
+
+    
+    if [[ ${have_compiler} -eq 0 ]] ; then
+        
+        # Create the assembly
+        $compiler -S $i -o ${working}/$base-got.s
+		mips-linux-gnu-gcc -static -o ${working}/$base-got ${working}/$base-got.s
+        
+        # Run the new
+        qemu-mips ${working}/$base-got
+        GOT_C_OUT=$?
+    fi
+    
+    if [[ ${have_compiler} -ne 0 ]] ; then
+        echo "$base, Fail, No C compiler/translator"
+    elif [[ $REF_C_OUT -ne $GOT_C_OUT ]] ; then
+        echo "$base, Fail, Expected ${REF_C_OUT}, got ${GOT_P_OUT}"
+    else
+        echo "$base, Pass"
+    fi
+done
