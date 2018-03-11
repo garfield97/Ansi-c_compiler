92f2f6d84094a6b93229fe9663707ae2ece75365
mahudu97 <mahuduhd@gmail.com>
2018-03-11 16:03:34 +0000

    Revert "done"
    This reverts commit 34bf6ddca06c23da71ad66acf87ad942f9b71b56.

--- src/ast/ast_expression.hpp
+++ src/ast/ast_expression.hpp
@@ -673,7 +673,7 @@ class expr_unary : public Node {
     //           | SIZEOF L_BRACKET NAME_TYPE R_BRACKET  
     private:
         std::string terminal;
-		NodePtr O_U;
+		opr_unary* O_U;
         NodePtr exp;
         
     public:
