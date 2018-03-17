0bcfea9ce82e498ebd5c21df55cfb7e3fdd28aec
garfield97 <rz1616@ic.ac.uk>
2018-03-17 15:42:34 +0000

    Mac > XPS

--- test_internal/switch.c
+++ test_internal/switch.c
@@ -2,27 +2,28 @@
 int main () {
 
    char grade = 'B';
+   int x;
 
    switch(grade) {
    
       case 'A' :
-            int x=1;
+            x=1;
             
       case 'B' :
-            int x=2;
+            x=2;
       
       case 'C' :
-            int x=3;
+            x=3;
             
       case 'D' :
-            int x=4;
+            x=4;
 
       case 'F' :
-            int x=5;
+            x=5;
 
 
       default :
-            int x=9999;
+            x=9999;
    }
    
     return x;  
