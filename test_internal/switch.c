10f4aac96b0a04c0472ca4d94a40a020a83fd9fb
garfield97 <rz1616@ic.ac.uk>
2018-03-17 15:40:24 +0000

    switch cases tests

---
+++ test_internal/switch.c
@@ -0,0 +1,31 @@
+
+int main () {
+
+   char grade = 'B';
+
+   switch(grade) {
+   
+      case 'A' :
+            int x=1;
+            
+      case 'B' :
+            int x=2;
+      
+      case 'C' :
+            int x=3;
+            
+      case 'D' :
+            int x=4;
+
+      case 'F' :
+            int x=5;
+
+
+      default :
+            int x=9999;
+   }
+   
+    return x;  
+ }
+
+
