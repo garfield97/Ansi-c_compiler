b4427eaa25c31079e658771cef9330e0321c6e6f
oscar <rz1616@ic.ac.uk>
2018-03-26 03:17:38 -0700

    atof test for testbench marks

---
+++ test_internal/atof.c
@@ -0,0 +1,12 @@
+
+int main () {
+   
+   float val;
+   char str[20];
+   
+   strcpy(str, "98993489");
+   val = atof(str);
+   printf("String value = %s, Float value = %f\n", str, val);
+
+   return(0);
+}
